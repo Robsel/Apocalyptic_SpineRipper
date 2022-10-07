5069135175714949a604d6778bc1fe1a26da03a5
Robsel <Robselsten@gmail.com>
2022-10-07 09:44:37 +0200

    energy crystal

---
+++ src/main/java/com/robsel/asmain/entity/client/cultistModel.java
@@ -0,0 +1,23 @@
+package com.robsel.asmain.entity.client;
+
+import com.robsel.asmain.ASMain;
+import com.robsel.asmain.entity.custom.gtent;
+import net.minecraft.resources.ResourceLocation;
+import software.bernie.geckolib3.model.AnimatedGeoModel;
+
+public class cultistModel extends AnimatedGeoModel<gtent> {
+    @Override
+    public ResourceLocation getModelLocation(gtent object) {
+        return new ResourceLocation(ASMain.MOD_ID, "geo/gtent.geo.json");
+    }
+
+    @Override
+    public ResourceLocation getTextureLocation(gtent object) {
+        return new ResourceLocation(ASMain.MOD_ID, "textures/entity/gtent.png");
+    }
+
+    @Override
+    public ResourceLocation getAnimationFileLocation(gtent animatable) {
+        return new ResourceLocation(ASMain.MOD_ID, "animations/gtent.animation.json");
+    }
+}
